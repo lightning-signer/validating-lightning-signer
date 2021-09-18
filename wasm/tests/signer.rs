c0937556e5620ae888bcc119d7b45ba3229a8760
Ken Sedgwick <ken@bonsai.com>
2021-09-17 17:11:29 -0700

    cargo fmt

--- wasm/tests/signer.rs
+++ wasm/tests/signer.rs
@@ -24,20 +24,14 @@ fn channel_test() {
         JSPublicKey::new_test_key(104),
     );
     let outpoint = JSOutPoint::default();
-    let setup = JSChannelSetup::new(
-        false,
-        10000,
-        0,
-        outpoint,
-        6,
-        cp_keys,
-        6,
-    );
+    let setup = JSChannelSetup::new(false, 10000, 0, outpoint, 6, cp_keys, 6);
     node.ready_channel(&channel_id, &setup).unwrap();
-    let _sig1 = node.sign_holder_commitment(
-        &channel_id,
-        0, // Commitment number
-        1, // to holder
-        0, // to counterparty
-    ).unwrap();
+    let _sig1 = node
+        .sign_holder_commitment(
+            &channel_id,
+            0, // Commitment number
+            1, // to holder
+            0, // to counterparty
+        )
+        .unwrap();
 }
