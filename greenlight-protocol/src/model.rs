c7848509eee65efd251a44c105592ab28bb9bf2c
Devrandom <c1.devrandom@niftybox.net>
2021-09-25 11:03:44 +0200

    greenlight: macroize long-slice primitives

--- greenlight-protocol/src/model.rs
+++ greenlight-protocol/src/model.rs
@@ -22,66 +22,78 @@ pub struct Secret([u8; 32]);
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PrivKey([u8; 32]);
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PubKey32([u8; 32]);
+
+macro_rules! array_impl {
+    ($ty:ident, $len:tt) => {
+        impl Debug for $ty {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                self.0.to_vec().fmt(f)
+            }
+        }
+
+        impl<'de> de::Deserialize<'de> for $ty {
+            fn deserialize<D>(d: D) -> core::result::Result<Self, D::Error> where D: de::Deserializer<'de> {
+                struct Visitor {
+                    marker: PhantomData<$ty>,
+                }
+
+                impl<'de> de::Visitor<'de> for Visitor
+                {
+                    type Value = $ty;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str(concat!("an array of length {}", $len))
+                    }
+
+                    #[inline]
+                    fn visit_seq<A>(self, mut seq: A) -> core::result::Result<Self::Value, A::Error>
+                        where
+                            A: de::SeqAccess<'de>,
+                    {
+                        let mut buf = [0u8; $len];
+                        for i in 0..buf.len() {
+                            let next = seq.next_element()?;
+                            buf[i] = match next {
+                                None => return Err(de::Error::invalid_length($len, &self)),
+                                Some(val) => val,
+                            };
+                        }
+                        Ok($ty(buf))
+                    }
+                }
+
+                impl Visitor {
+                    fn new() -> Self {
+                        Self {
+                            marker: PhantomData,
+                        }
+                    }
+                }
+                d.deserialize_tuple($len, Visitor::new())
+            }
+        }
+
+        impl ser::Serialize for $ty {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+                let mut tuple = serializer.serialize_tuple(self.0.len())?;
+                for i in 0..self.0.len() {
+                    tuple.serialize_element(&self.0[i])?;
+                }
+                tuple.end()
+            }
+        }
+    }
+}
+
 pub struct PubKey([u8; 33]);
 
-impl Debug for PubKey {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.0.to_vec().fmt(f)
-    }
-}
+array_impl!(PubKey, 33);
 
-struct PubKeyVisitor {
-    marker: PhantomData<PubKey>,
-}
+pub struct ExtKey([u8; 78]);
 
-impl<'de> de::Visitor<'de> for PubKeyVisitor
-{
-    type Value = PubKey;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str(concat!("an array of length 33"))
-    }
-
-    #[inline]
-    fn visit_seq<A>(self, mut seq: A) -> core::result::Result<Self::Value, A::Error>
-        where
-            A: de::SeqAccess<'de>,
-    {
-        let mut buf = [0u8; 33];
-        for i in 0..buf.len() {
-            let next = seq.next_element()?;
-            buf[i] = match next {
-                None => return Err(de::Error::invalid_length(33, &self)),
-                Some(val) => val,
-            };
-        }
-        Ok(PubKey(buf))
-    }
-}
-
-impl PubKeyVisitor {
-    fn new() -> Self {
-        Self {
-            marker: PhantomData,
-        }
-    }
-}
-
-impl<'de> de::Deserialize<'de> for PubKey {
-    fn deserialize<D>(d: D) -> core::result::Result<Self, D::Error> where D: de::Deserializer<'de> {
-        d.deserialize_tuple(33, PubKeyVisitor::new())
-    }
-}
-
-impl ser::Serialize for PubKey {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        let mut tuple = serializer.serialize_tuple(self.0.len())?;
-        for i in 0..self.0.len() {
-            tuple.serialize_element(&self.0[i])?;
-        }
-        tuple.end()
-    }
-}
+array_impl!(ExtKey, 78);
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Sha256([u8; 32]);
