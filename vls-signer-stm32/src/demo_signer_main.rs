210dd43b3e7914a0d2b21464c3247e24f8afc773
Devrandom <c1.devrandom@niftybox.net>
2022-04-15 19:46:58 +0200

    stm32: sdcard, cleanup

--- vls-signer-stm32/src/demo_signer_main.rs
+++ vls-signer-stm32/src/demo_signer_main.rs
@@ -22,6 +22,7 @@ use stm32f4xx_hal::{
     gpio::Speed,
     pac::{CorePeripherals, Peripherals},
     prelude::*,
+    sdio::{ClockFreq, SdCard, Sdio},
 };
 
 use profont::PROFONT_24_POINT;
@@ -40,102 +41,135 @@ fn main() -> ! {
     rtt_init_print!(NoBlockTrim);
     rprintln!("demo_signer starting");
 
-    if let (Some(p), Some(cp)) = (Peripherals::take(), CorePeripherals::take()) {
-        let rcc = p.RCC.constrain();
+    let p = Peripherals::take().unwrap();
+    let cp = CorePeripherals::take().unwrap();
+    let rcc = p.RCC.constrain();
 
-        // Make HCLK faster to allow updating the display more quickly
-        let clocks = rcc.cfgr.hclk(100.MHz()).freeze();
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.MHz())
+        .require_pll48clk()
+        .sysclk(100.MHz())
+        .hclk(100.MHz())
+        .pclk1(50.MHz())
+        .pclk2(100.MHz())
+        .freeze();
 
-        let mut delay = cp.SYST.delay(&clocks);
+    let mut delay = cp.SYST.delay(&clocks);
 
-        #[cfg(feature = "stm32f413")]
-        let gpiob = p.GPIOB.split();
+    #[cfg(feature = "stm32f413")]
+    let gpiob = p.GPIOB.split();
 
-        // both
-        let gpiod = p.GPIOD.split();
-        let gpioe = p.GPIOE.split();
-        let gpiof = p.GPIOF.split();
+    // both
+    let gpioa = p.GPIOA.split();
+    let gpioc = p.GPIOC.split();
+    let gpiod = p.GPIOD.split();
+    let gpioe = p.GPIOE.split();
+    let gpiof = p.GPIOF.split();
 
-        #[cfg(feature = "stm32f413")]
-        let gpiog = p.GPIOG.split();
+    #[cfg(feature = "stm32f413")]
+    let gpiog = p.GPIOG.split();
 
-        let lcd_pins = LcdPins {
-            data: (
-                gpiod.pd14.into_alternate(),
-                gpiod.pd15.into_alternate(),
-                gpiod.pd0.into_alternate(),
-                gpiod.pd1.into_alternate(),
-                gpioe.pe7.into_alternate(),
-                gpioe.pe8.into_alternate(),
-                gpioe.pe9.into_alternate(),
-                gpioe.pe10.into_alternate(),
-                gpioe.pe11.into_alternate(),
-                gpioe.pe12.into_alternate(),
-                gpioe.pe13.into_alternate(),
-                gpioe.pe14.into_alternate(),
-                gpioe.pe15.into_alternate(),
-                gpiod.pd8.into_alternate(),
-                gpiod.pd9.into_alternate(),
-                gpiod.pd10.into_alternate(),
-            ),
-            address: gpiof.pf0.into_alternate(),
-            read_enable: gpiod.pd4.into_alternate(),
-            write_enable: gpiod.pd5.into_alternate(),
-
-            #[cfg(feature = "stm32f412")]
-            chip_select: ChipSelect1(gpiod.pd7.into_alternate()),
-            #[cfg(feature = "stm32f413")]
-            chip_select: ChipSelect3(gpiog.pg10.into_alternate()),
-        };
-
-        #[cfg(feature = "stm32f412")]
-        let lcd_reset = gpiod.pd11.into_push_pull_output().speed(Speed::VeryHigh);
-        #[cfg(feature = "stm32f413")]
-        let lcd_reset = gpiob.pb13.into_push_pull_output().speed(Speed::VeryHigh);
-
-        #[cfg(feature = "stm32f412")]
-        let mut backlight_control = gpiof.pf5.into_push_pull_output();
-        #[cfg(feature = "stm32f413")]
-        let mut backlight_control = gpioe.pe5.into_push_pull_output();
-
-        let write_timing = Timing::default().data(3).address_setup(3).bus_turnaround(0);
-        let read_timing = Timing::default().data(8).address_setup(8).bus_turnaround(0);
-
-        let (_fsmc, interface) = FsmcLcd::new(p.FSMC, lcd_pins, &read_timing, &write_timing);
-
-        let mut disp = ST7789::new(interface, lcd_reset, 240, 240);
-
-        disp.init(&mut delay).unwrap();
+    let lcd_pins = LcdPins {
+        data: (
+            gpiod.pd14.into_alternate(),
+            gpiod.pd15.into_alternate(),
+            gpiod.pd0.into_alternate(),
+            gpiod.pd1.into_alternate(),
+            gpioe.pe7.into_alternate(),
+            gpioe.pe8.into_alternate(),
+            gpioe.pe9.into_alternate(),
+            gpioe.pe10.into_alternate(),
+            gpioe.pe11.into_alternate(),
+            gpioe.pe12.into_alternate(),
+            gpioe.pe13.into_alternate(),
+            gpioe.pe14.into_alternate(),
+            gpioe.pe15.into_alternate(),
+            gpiod.pd8.into_alternate(),
+            gpiod.pd9.into_alternate(),
+            gpiod.pd10.into_alternate(),
+        ),
+        address: gpiof.pf0.into_alternate(),
+        read_enable: gpiod.pd4.into_alternate(),
+        write_enable: gpiod.pd5.into_alternate(),
 
         #[cfg(feature = "stm32f412")]
-        disp.set_orientation(Orientation::Portrait).unwrap();
+        chip_select: ChipSelect1(gpiod.pd7.into_alternate()),
         #[cfg(feature = "stm32f413")]
-        disp.set_orientation(Orientation::PortraitSwapped).unwrap();
+        chip_select: ChipSelect3(gpiog.pg10.into_alternate()),
+    };
 
-        // Turn on backlight
-        backlight_control.set_high();
+    rprintln!("SDIO setup");
 
-        let mut format_buf = String::<20>::new();
-        let mut counter = 0;
-        let text_style =
-            MonoTextStyleBuilder::new().font(&PROFONT_24_POINT).text_color(Rgb565::WHITE).build();
+    let d0 = gpioc.pc8.into_alternate().internal_pull_up(true);
+    let d1 = gpioc.pc9.into_alternate().internal_pull_up(true);
+    let d2 = gpioc.pc10.into_alternate().internal_pull_up(true);
+    let d3 = gpioc.pc11.into_alternate().internal_pull_up(true);
+    let clk = gpioc.pc12.into_alternate().internal_pull_up(false);
+    let cmd = gpioa.pa6.into_alternate().internal_pull_up(true);
+    let mut sdio: Sdio<SdCard> = Sdio::new(p.SDIO, (clk, cmd, d0, d1, d2, d3), &clocks);
 
-        loop {
-            disp.clear(Rgb565::BLACK).unwrap();
+    #[cfg(feature = "stm32f412")]
+    let lcd_reset = gpiod.pd11.into_push_pull_output().speed(Speed::VeryHigh);
+    #[cfg(feature = "stm32f413")]
+    let lcd_reset = gpiob.pb13.into_push_pull_output().speed(Speed::VeryHigh);
 
-            format_buf.clear();
-            if fmt::write(&mut format_buf, format_args!("{}", counter)).is_ok() {
-                Text::new(&format_buf, Point::new(HINSET_PIX, VCENTER_PIX), text_style)
-                    .draw(&mut disp)
-                    .unwrap();
-            }
+    #[cfg(feature = "stm32f412")]
+    let mut backlight_control = gpiof.pf5.into_push_pull_output();
+    #[cfg(feature = "stm32f413")]
+    let mut backlight_control = gpioe.pe5.into_push_pull_output();
 
-            delay.delay_ms(100u16);
-            counter += 1;
+    let write_timing = Timing::default().data(3).address_setup(3).bus_turnaround(0);
+    let read_timing = Timing::default().data(8).address_setup(8).bus_turnaround(0);
+
+    let (_fsmc, interface) = FsmcLcd::new(p.FSMC, lcd_pins, &read_timing, &write_timing);
+
+    let mut disp = ST7789::new(interface, lcd_reset, 240, 240);
+
+    disp.init(&mut delay).unwrap();
+
+    #[cfg(feature = "stm32f412")]
+    disp.set_orientation(Orientation::Portrait).unwrap();
+    #[cfg(feature = "stm32f413")]
+    disp.set_orientation(Orientation::PortraitSwapped).unwrap();
+
+    // Turn on backlight
+    backlight_control.set_high();
+
+    let mut format_buf = String::<20>::new();
+    let mut counter = 0;
+    let text_style =
+        MonoTextStyleBuilder::new().font(&PROFONT_24_POINT).text_color(Rgb565::WHITE).build();
+
+    rprintln!("detecting sdcard");
+    loop {
+        match sdio.init(ClockFreq::F24Mhz) {
+            Ok(_) => break,
+            Err(e) => rprintln!("waiting for sdio - {:?}", e),
         }
+
+        delay.delay_ms(1000u32);
     }
 
+    let nblocks = sdio.card().map(|c| c.block_count());
+    rprintln!("sdcard detected: nbr of blocks: {:?}", nblocks);
+
+    let mut block = [0u8; 512];
+
+    let res = sdio.read_block(0, &mut block);
+    rprintln!("sdcard read result {:?}", res);
+
     loop {
-        continue;
+        disp.clear(Rgb565::BLACK).unwrap();
+
+        format_buf.clear();
+        if fmt::write(&mut format_buf, format_args!("{}", counter)).is_ok() {
+            Text::new(&format_buf, Point::new(HINSET_PIX, VCENTER_PIX), text_style)
+                .draw(&mut disp)
+                .unwrap();
+        }
+
+        delay.delay_ms(100u16);
+        counter += 1;
     }
 }
