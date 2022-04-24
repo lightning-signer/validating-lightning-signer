81d20b984d1ed37213922c10bf1100edfb7d6c52
Devrandom <c1.devrandom@niftybox.net>
2022-04-24 06:43:28 -0700

    stm32: clean up serial

--- vls-signer-stm32/src/usbserial.rs
+++ vls-signer-stm32/src/usbserial.rs
@@ -1,6 +1,5 @@
 use alloc::{collections::VecDeque, vec::Vec};
-use core::cell::{RefCell, RefMut};
-use core::ops::DerefMut;
+use core::cell::RefCell;
 use cortex_m::interrupt::{free, Mutex};
 use stm32f4xx_hal::{
     interrupt,
@@ -15,12 +14,16 @@ use usbd_serial::SerialPort;
 use log::{debug, error};
 
 static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+pub static SERIAL: Mutex<RefCell<Option<SerialDriver>>> = Mutex::new(RefCell::new(None));
+static mut USB_BUS: Option<UsbBusAllocator<UsbBus<USB>>> = None;
 
-static mut USB_BUS: Option<UsbBusAllocator<stm32f4xx_hal::otg_fs::UsbBus<USB>>> = None;
-static mut SERIAL: Option<SerialPort<UsbBus<USB>>> = None;
-static mut USB_DEV: Option<UsbDevice<UsbBus<USB>>> = None;
-
-static TIMER_TIM2: Mutex<RefCell<Option<CounterUs<TIM2>>>> = Mutex::new(RefCell::new(None));
+pub struct SerialDriver {
+    serial: SerialPort<'static, UsbBus<USB>>,
+    usb_dev: UsbDevice<'static, UsbBus<USB>>,
+    timer_tim2: CounterUs<TIM2>,
+    inbuf: InputBuffer,
+    outbuf: OutputBuffer,
+}
 
 const READ_BUFSZ: usize = 1024;
 
@@ -41,103 +44,90 @@ impl InputBuffer {
     }
 }
 
-static INBUF: Mutex<RefCell<InputBuffer>> =
-    Mutex::new(RefCell::new(InputBuffer { data: [0; READ_BUFSZ], size: 0 }));
-
 struct OutputBuffer {
-    data: Option<VecDeque<u8>>,
+    data: VecDeque<u8>,
 }
 
-static OUTBUF: Mutex<RefCell<OutputBuffer>> = Mutex::new(RefCell::new(OutputBuffer { data: None }));
+impl SerialDriver {
+    pub(crate) fn init(usb: USB, timer: CounterUs<TIM2>) {
+        let inbuf = InputBuffer { data: [0; READ_BUFSZ], size: 0 };
+        let outbuf = OutputBuffer { data: VecDeque::new() };
 
-pub fn init(usb: USB, timer: CounterUs<TIM2>) {
-    // This is called once on startup
-    unsafe {
-        USB_BUS = Some(UsbBus::new(usb, &mut EP_MEMORY));
+        // This is called once on startup
+        unsafe {
+            assert!(USB_BUS.is_none());
+            USB_BUS = Some(UsbBus::new(usb, &mut EP_MEMORY));
+        };
 
-        SERIAL = Some(SerialPort::new(USB_BUS.as_mut().unwrap()));
+        let serial = unsafe { SerialPort::new(USB_BUS.as_ref().unwrap()) };
 
-        USB_DEV = Some(
-            UsbDeviceBuilder::new(USB_BUS.as_mut().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+        let usb_dev = unsafe {
+            UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
                 .manufacturer("VLS")
                 .product("signer")
                 .serial_number("TEST")
                 .device_class(usbd_serial::USB_CLASS_CDC)
-                .build(),
-        );
+                .build()
+        };
+
+        // Enable interrupts
+        NVIC::unpend(Interrupt::TIM2);
+        unsafe {
+            NVIC::unmask(Interrupt::TIM2);
+        };
+        let serial_driver =
+            SerialDriver { serial: serial, usb_dev, timer_tim2: timer, inbuf, outbuf };
+        free(|cs| SERIAL.borrow(cs).replace(Some(serial_driver)));
     }
-
-    free(|cs| {
-        TIMER_TIM2.borrow(cs).replace(Some(timer));
-        OUTBUF.borrow(cs).borrow_mut().data = Some(VecDeque::new());
-    });
-
-    // Enable interrupts
-    NVIC::unpend(Interrupt::TIM2);
-    unsafe {
-        NVIC::unmask(Interrupt::TIM2);
-    };
 }
 
 #[interrupt]
 fn TIM2() {
     free(|cs| {
-        if let Some(ref mut tim2) = TIMER_TIM2.borrow(cs).borrow_mut().deref_mut() {
-            tim2.clear_interrupt(Event::Update);
-        }
+        let mut serial_lock = SERIAL.borrow(cs).borrow_mut();
+        let serial = serial_lock.as_mut().unwrap();
+        serial.timer_tim2.clear_interrupt(Event::Update);
 
-        let mut inbuf = INBUF.borrow(cs).borrow_mut();
-        append_inbuf(&mut inbuf);
+        serial.append_inbuf();
 
-        let mut outbuf = OUTBUF.borrow(cs).borrow_mut();
-        drain_outbuf(&mut outbuf);
+        serial.drain_outbuf();
     });
 }
 
-pub fn read() -> Option<Vec<u8>> {
-    free(|cs| {
-        let mut inbuf = INBUF.borrow(cs).borrow_mut();
-        append_inbuf(&mut inbuf);
-        inbuf.harvest()
-    })
-}
+impl SerialDriver {
+    pub fn read(&mut self) -> Option<Vec<u8>> {
+        self.append_inbuf();
+        self.inbuf.harvest()
+    }
 
-pub fn write(outgoing: &[u8]) {
-    free(|cs| {
-        let mut outbuf = OUTBUF.borrow(cs).borrow_mut();
-        outbuf.data.as_mut().unwrap().extend(outgoing);
-        drain_outbuf(&mut outbuf);
-    })
-}
+    pub fn write(&mut self, outgoing: &[u8]) {
+        self.outbuf.data.extend(outgoing);
+        self.drain_outbuf();
+    }
 
-fn append_inbuf(inbuf: &mut RefMut<'_, InputBuffer>) {
-    // These are not modified after init
-    let (usb_dev, serial) = unsafe { (USB_DEV.as_mut().unwrap(), SERIAL.as_mut().unwrap()) };
-
-    let size = inbuf.size;
-    if size < inbuf.data.len() {
-        if usb_dev.poll(&mut [serial]) {
-            match serial.read(&mut inbuf.data[size..]) {
-                Ok(count) => inbuf.size += count,
-                Err(UsbError::WouldBlock) => {}
-                Err(err) => error!("append_inbuf: serial.read error: {:?}", err),
+    fn append_inbuf(&mut self) {
+        let size = self.inbuf.size;
+        if size < self.inbuf.data.len() {
+            if self.usb_dev.poll(&mut [&mut self.serial]) {
+                match self.serial.read(&mut self.inbuf.data[size..]) {
+                    Ok(count) => self.inbuf.size += count,
+                    Err(UsbError::WouldBlock) => {}
+                    Err(err) => error!("append_inbuf: serial.read error: {:?}", err),
+                }
             }
         }
     }
-}
 
-fn drain_outbuf(outbuf: &mut RefMut<'_, OutputBuffer>) {
-    // These are not modified after init
-    let serial = unsafe { SERIAL.as_mut().unwrap() };
-
-    let ovd = &mut outbuf.data.as_mut().unwrap();
-    if !ovd.is_empty() {
-        match serial.write(ovd.make_contiguous()) {
-            Ok(count) => {
-                ovd.drain(0..count);
+    fn drain_outbuf(&mut self) {
+        let ovd = &mut self.outbuf.data;
+        if !ovd.is_empty() {
+            match self.serial.write(ovd.make_contiguous()) {
+                Ok(count) => {
+                    ovd.drain(0..count);
+                }
+                Err(UsbError::WouldBlock) => {}
+                Err(err) => error!("drain_outbuf: serial.write error: {:?}", err),
             }
-            Err(UsbError::WouldBlock) => {}
-            Err(err) => error!("drain_outbuf: serial.write error: {:?}", err),
         }
     }
 }
