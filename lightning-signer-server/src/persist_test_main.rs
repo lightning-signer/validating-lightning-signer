fdc0ee3e5ee8933d0b329f3153f17b7510db16f9
Devrandom <c1.devrandom@niftybox.net>
2022-07-07 23:07:46 +0200

    clean up test_utils handling in CI, fix out of date test binary

--- lightning-signer-server/src/persist_test_main.rs
+++ lightning-signer-server/src/persist_test_main.rs
@@ -1,29 +1,26 @@
 use kv::Json;
 
-use lightning_signer::channel::channel_nonce_to_id;
+use lightning_signer::channel::ChannelId;
 use lightning_signer::persist::Persist;
-use lightning_signer::util::test_utils::TEST_NODE_CONFIG;
+use lightning_signer::util::test_utils::{self, hex_decode, TEST_CHANNEL_ID, TEST_NODE_CONFIG};
 use lightning_signer_server::persist::model::{ChannelEntry, NodeChannelId, NodeEntry};
 use lightning_signer_server::persist::persist_json::KVJsonPersister;
-use lightning_signer_server::persist::util;
 
 pub fn main() {
     let persister = KVJsonPersister::new("/tmp/signer.kv");
     persister.clear_database();
-    let channel_nonce = "nonce0".as_bytes().to_vec();
-    let channel_id = channel_nonce_to_id(&channel_nonce);
-    let channel_nonce1 = "nonce1".as_bytes().to_vec();
-    let channel_id1 = channel_nonce_to_id(&channel_nonce1);
+    let channel_id = ChannelId::new(&hex_decode(TEST_CHANNEL_ID[0]).unwrap());
+    let channel_id1 = ChannelId::new(&hex_decode(TEST_CHANNEL_ID[1]).unwrap());
 
-    let (node_id, node_arc, stub, _seed) = util::make_node_and_channel(&channel_nonce, channel_id);
+    let (node_id, node_arc, stub, _seed) = test_utils::make_node_and_channel(channel_id.clone());
     let node = &*node_arc;
 
     persister.new_node(&node_id, &TEST_NODE_CONFIG, &[3u8; 32]);
 
     persister.new_channel(&node_id, &stub).unwrap();
 
-    let dummy_pubkey = util::make_dummy_pubkey(0x12);
-    let setup = util::create_test_channel_setup(dummy_pubkey);
+    let dummy_pubkey = test_utils::make_dummy_pubkey(0x12);
+    let setup = test_utils::create_test_channel_setup(dummy_pubkey);
     let channel = node.ready_channel(channel_id, Some(channel_id1), setup, &vec![]).unwrap();
 
     for (id, entry) in persister.get_node_channels(&node_id) {
