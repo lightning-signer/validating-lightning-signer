e33cc343867ce12b94f49248690d03a19e9ec873
Ken Sedgwick <ken@bonsai.com>
2022-07-21 16:54:09 -0700

    Adjust mainnet policy parameters

--- lightning-signer-core/src/policy/simple_validator.rs
+++ lightning-signer-core/src/policy/simple_validator.rs
@@ -1735,15 +1735,15 @@ impl SimpleValidator {
 pub fn make_simple_policy(network: Network) -> SimplePolicy {
     if network == Network::Bitcoin {
         SimplePolicy {
-            min_delay: 60,
-            max_delay: 2016, // Match LDK maximum and default
+            min_delay: 144,  // LDK min
+            max_delay: 2016, // LDK max
             max_channel_size_sat: 1_000_000_001,
-            epsilon_sat: 1_600_000,
+            epsilon_sat: 10_000,
             max_htlcs: 1000,
             max_htlc_value_sat: 16_777_216,
             use_chain_state: false,
-            min_feerate_per_kw: 253, // mainnet observed
-            max_feerate_per_kw: 1000 * 1000,
+            min_feerate_per_kw: 253,    // mainnet observed
+            max_feerate_per_kw: 25_000, // equiv to 100 sat/vb
             require_invoices: false,
             enforce_balance: false,
             max_routing_fee_msat: 10000,
