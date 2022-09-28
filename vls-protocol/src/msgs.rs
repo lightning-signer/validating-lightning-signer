c034681723a7ce18273b174a96eb2cd130177e00
Devrandom <c1.devrandom@niftybox.net>
2022-09-28 16:14:57 +0200

    protocol: Persist message

--- vls-protocol/src/msgs.rs
+++ vls-protocol/src/msgs.rs
@@ -620,6 +620,29 @@ pub struct RemoveBlock {
 #[message_id(2106)]
 pub struct RemoveBlockReply {}
 
+/// Store key-value pairs to persistent storage - potentially in the cloud.
+/// This message may be sent by the *signer* in response to a normal API message,
+/// before the actual API response.  The front end should reply with [`PersistReply`]
+#[derive(SerBolt, Debug, Serialize, Deserialize)]
+#[message_id(2107)]
+pub struct Persist {
+    /// Authentication token from client (signer) to storage service
+    pub auth: Vec<u8>,
+    pub kvs: Vec<(Vec<u8>, u64, LargeBytes)>,
+    /// HMAC by client to authenticate the message
+    pub hmac: Vec<u8>,
+}
+
+/// Result of a [`Persist`].
+#[derive(SerBolt, Debug, Serialize, Deserialize)]
+#[message_id(2007)]
+pub struct PersistReply {
+    pub success: bool,
+    /// HMAC by storage service to authenticate the message
+    pub hmac: Vec<u8>,
+    pub conflicts: Vec<(Vec<u8>, u64, LargeBytes)>,
+}
+
 /// An unknown message
 #[derive(Debug, Serialize)]
 pub struct Unknown {
@@ -703,6 +726,8 @@ pub enum Message {
     AddBlockReply(AddBlockReply),
     RemoveBlock(RemoveBlock),
     RemoveBlockReply(RemoveBlockReply),
+    Persist(Persist),
+    PersistReply(PersistReply),
     Unknown(Unknown),
 }
 
@@ -892,6 +917,33 @@ mod tests {
         if let Message::SignChannelAnnouncementReply(dmsg) = dmsg {
             assert_eq!(dmsg.node_signature.0, msg.node_signature.0);
             assert_eq!(dmsg.bitcoin_signature.0, msg.bitcoin_signature.0);
+        } else {
+            panic!("bad deser type")
+        }
+    }
+
+    // Test the persist message, since it has more more nesting than others
+    #[test]
+    fn persist_test() {
+        let key = b"foo".to_vec();
+        let value = b"bar".to_vec();
+        let version = 0x123456789;
+        let msg = Persist {
+            auth: vec![0x11, 0x22],
+            kvs: vec![(key.clone(), version, LargeBytes(value.clone()))],
+            hmac: vec![0x33, 0x44],
+        };
+
+        let ser = msg.as_vec();
+        let dmsg = from_vec(ser).unwrap();
+        if let Message::Persist(dmsg) = dmsg {
+            assert_eq!(dmsg.auth, msg.auth);
+            for (k, ver, v) in dmsg.kvs.into_iter() {
+                assert_eq!(k, key);
+                assert_eq!(ver, version);
+                assert_eq!(v.0, value);
+            }
+            assert_eq!(dmsg.hmac, msg.hmac);
         } else {
             panic!("bad deser type")
         }
