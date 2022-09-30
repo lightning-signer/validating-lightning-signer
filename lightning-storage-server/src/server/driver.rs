338cad808416217c9dc22632e286c743eccf7cf1
Christian Decker <decker.christian@gmail.com>
2022-09-30 16:20:58 +0200

    lss: Add TLS support for the grpc interface
    Adding TLS support should allow us to prevent eavesdroppers from
    snooping on node metadata.

--- lightning-storage-server/src/server/driver.rs
+++ lightning-storage-server/src/server/driver.rs
@@ -7,8 +7,32 @@ use crate::database::sled::SledDatabase;
 use crate::server::LightningStorageServer;
 use crate::server::StorageServer;
 use crate::util::{init_secret_key, read_public_key, read_secret_key, setup_logging};
+use tonic::transport::{server::ServerTlsConfig, Identity};
 
 pub const SERVER_APP_NAME: &str = "lssd";
+
+fn configure_tls(
+    server: tonic::transport::Server,
+    matches: &clap::ArgMatches,
+    datadir: &std::path::PathBuf,
+) -> Result<tonic::transport::Server, Box<dyn std::error::Error>> {
+    if !matches.is_present("grpc-tls-key") {
+        return Ok(server);
+    }
+
+    let mut key_file = datadir.clone();
+    key_file.push(matches.value_of("grpc-tls-key").unwrap());
+    let key = std::fs::read(key_file).map_err(|_| "could not read key file")?;
+
+    let mut cert_file = home_dir;
+    cert_file.push(matches.value_of("grpc-tls-certificate").unwrap());
+    let cert = std::fs::read(cert_file).map_err(|_| "could not read certificate file")?;
+
+    let identity = Identity::from_pem(cert, key);
+    let tls_config = ServerTlsConfig::new().identity(identity);
+
+    Ok(server.tls_config(tls_config)?)
+}
 
 #[tokio::main(worker_threads = 2)]
 pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
@@ -31,6 +55,20 @@ pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
                 .long("port")
                 .takes_value(true)
                 .default_value("55551"),
+        )
+        .arg(
+            Arg::new("grpc-tls-certificate")
+                .about("Server identity certificate")
+                .long("grpc-tls-certificate")
+                .takes_value(true)
+                .requires("grpc-tls-key"),
+        )
+        .arg(
+            Arg::new("grpc-tls-key")
+                .long("grpc-tls-key")
+                .about("Server identity key")
+                .takes_value(true)
+                .requires("grpc-tls-certificate"),
         )
         .arg(
             Arg::new("datadir")
@@ -66,7 +104,9 @@ pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
     })
     .expect("Error setting Ctrl-C handler");
 
-    let service = tonic::transport::Server::builder()
+    let service = tonic::transport::Server::builder();
+
+    let service = configure_tls(service, &matches, &datadir)?
         .add_service(LightningStorageServer::new(server))
         .serve_with_shutdown(addr, shutdown_signal);
     info!("{} {} ready on {} datadir {}", SERVER_APP_NAME, process::id(), addr, datadir.display());
