ab2d3bc77f056770f3fbca20cc43b2bc6c9b220c
Ken Sedgwick <ken@bonsai.com>
2022-12-08 14:21:17 -0800

    stm32: Improve version string generation wrt dependencies

--- vls-signer-stm32/build.rs
+++ vls-signer-stm32/build.rs
@@ -1,17 +1,34 @@
+use std::env;
+use std::fs;
+use std::path::Path;
 use std::process::Command;
 
-fn main() {
+fn update_version() {
     let git_desc = match Command::new("git")
         .args(&["describe", "--tags", "--long", "--always", "--match=v*.*", "--dirty"])
         .output()
     {
         Ok(output) =>
             if output.status.success() {
-                String::from_utf8(output.stdout).unwrap_or("git-desc-badstr".to_string())
+                String::from_utf8(output.stdout)
+                    .unwrap_or("git-desc-badstr".to_string())
+                    .trim_end()
+                    .to_string()
             } else {
                 "git-desc-error".to_string()
             },
         Err(_) => "git-desc-failed".to_string(),
     };
-    println!("cargo:rustc-env=GIT_DESC={}", git_desc);
+    let version_source = format!("pub const GIT_DESC: &'static str = \"{}\";\n", git_desc);
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let version_path = Path::new(&out_dir).join("version.rs");
+    let old_version_source =
+        String::from_utf8(fs::read(&version_path).unwrap_or(vec![])).unwrap_or("".to_string());
+    if version_source != old_version_source {
+        fs::write(&version_path, version_source).unwrap();
+    }
+}
+
+fn main() {
+    update_version();
 }
