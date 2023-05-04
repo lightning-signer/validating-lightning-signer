088fb6c453d866bf2b3686db19e6fdc6a5d91042
Ken Sedgwick <ken@bonsai.com>
2023-05-03 19:05:40 -0700

    Use heapmon::peakhold and dump large excursions

--- vls-proxy/src/grpc/signer.rs
+++ vls-proxy/src/grpc/signer.rs
@@ -36,6 +36,11 @@ use vls_protocol_signer::handler::{Error, Handler, RootHandler, RootHandlerBuild
 use vls_protocol_signer::vls_protocol::model::PubKey;
 use vls_protocol_signer::vls_protocol::msgs;
 
+use heapmon::{self, HeapMon, SummaryOrder};
+use std::alloc::System;
+#[global_allocator]
+pub static HEAPMON: HeapMon<System> = HeapMon::system();
+
 /// Signer binary entry point for local integration test
 #[tokio::main(worker_threads = 2)]
 pub async fn start_signer_localhost(port: u16) {
@@ -124,11 +129,35 @@ async fn connect(datadir: &str, uri: Uri, args: &SignerArgs) {
 
     let mut request_stream = client.signer_stream(response_stream).await.unwrap().into_inner();
 
+    use std::env;
+    let peak_thresh = env::var("VLS_HEAPMON_PEAK_THRESH")
+        .map(|s| s.parse().expect("VLS_HEAPMON_PEAK_THRESH parse"))
+        .unwrap_or(50 * 1024);
+    info!("using VLS_HEAPMON_PEAK_THRESH={}", peak_thresh);
+    HEAPMON.filter("KVJsonPersister");
+    HEAPMON.filter("sled::pagecache");
+    HEAPMON.filter("backtrace::symbolize");
+    HEAPMON.filter("redb::");
+
     while let Some(item) = request_stream.next().await {
         match item {
             Ok(request) => {
                 let request_id = request.request_id;
+
+                // Enable peakhold for every message
+                HEAPMON.reset();
+                HEAPMON.peakhold();
+                let heapmon_label =
+                    msgs::from_vec(request.clone().message).expect("msg").inner().name();
+
                 let response = handle(request, &root_handler);
+
+                // But only dump big heap excursions
+                let (_heapsz, peaksz) = HEAPMON.disable();
+                if peaksz > peak_thresh {
+                    HEAPMON.dump(SummaryOrder::MemoryUsed, peak_thresh, heapmon_label);
+                }
+
                 match response {
                     Ok(response) => {
                         let res = sender.send(response).await;
@@ -226,6 +255,7 @@ fn get_or_generate_seed(
 
 fn handle(request: SignerRequest, root_handler: &RootHandler) -> StdResult<SignerResponse, Error> {
     let msg = msgs::from_vec(request.message)?;
+
     info!(
         "signer got request {} dbid {} - {:?}",
         request.request_id,
@@ -251,6 +281,7 @@ fn handle(request: SignerRequest, root_handler: &RootHandler) -> StdResult<Signe
     info!("signer sending reply {} - {:?}", request.request_id, reply);
     // TODO handle memorized mutations
     let (res, _muts) = reply;
+
     Ok(SignerResponse {
         request_id: request.request_id,
         message: res.as_vec(),
