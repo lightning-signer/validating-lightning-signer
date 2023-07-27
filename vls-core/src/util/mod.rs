5b9579f571568327641419f188ef38d0415c0ece
Devrandom <c1.devrandom@niftybox.net>
2023-07-27 10:33:26 +0200

    core: semi-streaming PSBT parser

--- vls-core/src/util/mod.rs
+++ vls-core/src/util/mod.rs
@@ -25,6 +25,8 @@ pub mod test_logger;
 pub mod test_utils;
 #[cfg(test)]
 pub(crate) mod mocks;
+/// Streaming PSBT
+pub mod psbt;
 /// serde for foreign types
 #[allow(missing_docs)]
 pub mod ser_util;
