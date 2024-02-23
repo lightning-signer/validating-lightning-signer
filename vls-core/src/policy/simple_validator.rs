c1fcd96b0738fb5e10a733d8dec3a1766ef70846
Ken Sedgwick <ken@bonsai.com>
2024-02-22 16:07:02 -0800

    ci: increase max_routing_fee to clear renepay test
    Changelog-None: nothing worth writing home about

--- vls-core/src/policy/simple_validator.rs
+++ vls-core/src/policy/simple_validator.rs
@@ -1894,7 +1894,7 @@ pub fn make_simple_policy(network: Network) -> SimplePolicy {
             min_feerate_per_kw: 253,     // testnet/regtest observed
             max_feerate_per_kw: 151_000, // CLN max_feerate 150_000 common
             enforce_balance: false,
-            max_routing_fee_msat: 120_000, // CLN test_pay_exclude_node: 101_000
+            max_routing_fee_msat: 160_000, // CLN test_renepay::test_fees: 153_000
             max_feerate_percentage: 10,
             dev_flags: None,
             filter: PolicyFilter::default(),
