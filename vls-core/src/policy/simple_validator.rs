3047daa7d7370a6591679766fb9095e672fcb442
Ken Sedgwick <ken@bonsai.com>
2024-05-27 16:46:41 -0700

    policy: increase max_routing_fee_msat because CLN test just outside

--- vls-core/src/policy/simple_validator.rs
+++ vls-core/src/policy/simple_validator.rs
@@ -1913,7 +1913,7 @@ pub fn make_simple_policy(network: Network) -> SimplePolicy {
             min_feerate_per_kw: 253,     // testnet/regtest observed
             max_feerate_per_kw: 151_000, // CLN max_feerate 150_000 common
             enforce_balance: false,
-            max_routing_fee_msat: 160_000, // CLN test_renepay::test_fees: 153_000
+            max_routing_fee_msat: 222_000, // CLN test_pay_avoid_low_fee_chan_1: 200_000
             max_feerate_percentage: 10,
             dev_flags: None,
             filter: PolicyFilter::default(),
