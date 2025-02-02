830e9f355d9330c256a3c35493ddb03b8983fb1d
Lakshya Singh <lakshay.singh1108@gmail.com>
2025-02-02 20:23:20 +0530

    proxy: update opentelemetry versions for MSRV 1.74.0
    - api deprecations, resolved
    - test tracer requires use of multi thread environment
    - update log formatting
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- vls-proxy/src/util/otlp.rs
+++ vls-proxy/src/util/otlp.rs
@@ -1,14 +1,11 @@
 use super::{deployment_environment, otlp_endpoint, otlp_timeout};
 use crate::GIT_DESC;
 use opentelemetry::KeyValue;
-use opentelemetry_otlp::{ExportConfig, Protocol, WithExportConfig};
-use opentelemetry_sdk::{
-    runtime,
-    trace::{self, BatchConfig, Tracer},
-    Resource,
-};
+use opentelemetry_otlp::{ExportConfig, Protocol, SpanExporter, WithExportConfig};
+use opentelemetry_sdk::{runtime, trace::TracerProvider as SdkTracerProvider, Resource};
 use opentelemetry_semantic_conventions::{
-    resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
+    attribute::DEPLOYMENT_NAME,
+    resource::{SERVICE_NAME, SERVICE_VERSION},
     SCHEMA_URL,
 };
 use std::time::Duration;
@@ -21,7 +18,7 @@ pub fn resource() -> Resource {
         [
             KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
             KeyValue::new(SERVICE_VERSION, GIT_DESC),
-            KeyValue::new(DEPLOYMENT_ENVIRONMENT, deployment_environment()),
+            KeyValue::new(DEPLOYMENT_NAME, deployment_environment()),
         ],
         SCHEMA_URL,
     )
@@ -38,35 +35,29 @@ pub fn otlp_exporter_config() -> ExportConfig {
     }
 }
 
-/** Initialize a tracer provider with the OTLP exporter and a batch span processor.*/
-pub fn new_tracer() -> Result<Tracer, opentelemetry::trace::TraceError> {
-    opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(
-            opentelemetry_otlp::new_exporter().tonic().with_export_config(otlp_exporter_config()),
-        )
-        .with_trace_config(trace::config().with_resource(resource()))
-        .with_batch_config(BatchConfig::default())
-        .install_batch(runtime::Tokio)
+/** Initialize a tracer provider for OTLP with a batch span exporter.*/
+pub fn new_tracer_provider() -> Result<SdkTracerProvider, opentelemetry::trace::TraceError> {
+    let span_exporter =
+        SpanExporter::builder().with_tonic().with_export_config(otlp_exporter_config()).build()?;
+
+    Ok(SdkTracerProvider::builder()
+        .with_batch_exporter(span_exporter, runtime::Tokio)
+        .with_resource(resource())
+        .build())
 }
 
 #[cfg(test)]
 mod tests {
     use opentelemetry::{
         global::ObjectSafeSpan,
-        trace::{SpanBuilder, Tracer},
+        trace::{SpanBuilder, Tracer, TracerProvider as _},
     };
 
-    #[tokio::test]
+    #[tokio::test(flavor = "multi_thread")]
     async fn test_new_tracer() {
-        let tracer = super::new_tracer();
-        match tracer {
-            Ok(tracer) => {
-                let mut _span = tracer.build(SpanBuilder::default());
-                tracing::info!("tracer test");
-                _span.end();
-            }
-            Err(err) => panic!("Failed to create tracer: {}", err),
-        }
+        let tracer_provider = super::new_tracer_provider().unwrap();
+        let mut _span = tracer_provider.tracer("test-tracer").build(SpanBuilder::default());
+        tracing::info!("tracer test");
+        _span.end();
     }
 }
