97bb785519ce304b29d00f114c0d93a11c8b8358
Lakshya Singh <lakshay.singh1108@gmail.com>
2025-08-08 06:46:34 +0530

    common: Add index derivation path utility
    Add to_derivation_path utility function to create a 
    DerivationPath from a vector of indices. This 
    helps with BIP32 path creation.
    
    Make bitcoin a workspace dependency for consistent versioning and add 
    it to vls-common. Import necessary bitcoin types in vls-common to 
    support the new functionality.

--- vls-common/src/lib.rs
+++ vls-common/src/lib.rs
@@ -3,6 +3,8 @@
 extern crate alloc;
 
 use alloc::string::String;
+use alloc::vec::Vec;
+use bitcoin::bip32::{ChildNumber, DerivationPath};
 
 #[macro_use]
 pub mod macros;
@@ -14,5 +16,26 @@ pub trait HexEncode {
 impl<T: hex::ToHex> HexEncode for T {
     fn to_hex(&self) -> String {
         self.encode_hex()
+    }
+}
+
+pub fn to_derivation_path<T: Into<u32> + Copy>(child_index: &[T]) -> DerivationPath {
+    child_index
+        .iter()
+        .map(|index| ChildNumber::from((*index).into()))
+        .collect::<Vec<ChildNumber>>()
+        .into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_index_derivation_path() {
+        let path: Vec<u32> = vec![1, 11 | (1 << 31), 3, 4];
+
+        let derivation_path = to_derivation_path(&path);
+        assert_eq!("1/11'/3/4", derivation_path.to_string());
     }
 }
